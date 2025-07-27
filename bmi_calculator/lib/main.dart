c2d7079e8aa1c30080694f11c70c9e02f9d54c74
unknown <you@example.com>
2025-07-27 11:53:27 +0600

    gender tile

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -12,45 +12,69 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            spacing: 20,
-            children: [
-              Text(
-                'BMI Calculator',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(height: 200, width: 200, color: Colors.green),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              spacing: 20,
+              children: [
+                Text(
+                  'BMI Calculator',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      child: Column(children: [Icon(Icons.male_sharp)]),
+                      height: 200,
+                      width: 200,
+                      color: Colors.green,
+                    ),
 
-                  Container(width: 200, height: 200, color: Colors.green),
-                ],
-              ),
-              Container(
-                height: 200,
-                width: double.infinity,
-                color: Colors.green,
-                child: Center(
-                  child: Column(
+                    Container(width: 200, height: 200, color: Colors.green),
+                  ],
+                ),
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Colors.green,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          'This is a container with a long text that should wrap properly.',
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
                     children: [
-                      Text(
-                        'This is a container with a long text that should wrap properly.',
-                        textAlign: TextAlign.center,
+                      Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.green,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'This is a container with a long text that should wrap properly.',
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
                       ),
+
+                      Container(height: 200, width: 200, color: Colors.green),
                     ],
                   ),
                 ),
-              ),
-              Row(
-                children: [
-                  Container(height: 200, width: 200, color: Colors.green),
-                  Container(height: 200, width: 200, color: Colors.green),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
